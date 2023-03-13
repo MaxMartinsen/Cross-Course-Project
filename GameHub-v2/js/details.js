4818735c9af6288bd30be44e2e45155c17ed911b
MaxMartinsen <max.martinsen@hotmail.com>
2023-03-13 20:14:25 +0100

    Create

---
+++ GameHub-v2/js/details.js
@@ -0,0 +1,31 @@
+const detailsContainer = document.querySelector(".details-container");
+detailsContainer.innerHTML = "";
+
+detailsContainer.classList.add("loader");
+
+const queryString = document.location.search;
+const params = new URLSearchParams(window.location.search);
+const id = parseInt(params.get("id"));
+
+setTimeout(() => {
+  detailsContainer.classList.remove("loader");
+
+  try {
+    const product = products.find(product => product.id === id);
+
+    detailsContainer.innerHTML = `<div class="details-image" style="background-image: url(${product.img});"></div>
+    <div class="details">
+      <h2>${product.game}</h2>
+      <p>Brand: ${product.brand}</p>
+      <h3>Description:</h3>
+      <p>${product.description}</p>
+      <h4>Price: ${product.price} $</h4>
+      <a href="#!" class="btn btn--buy">Buy now</a>
+    </div>
+    `;
+
+  } catch (error) {
+
+    detailsContainer.innerHTML = "An error occurred while loading the content.";
+  }
+}, 1000);
