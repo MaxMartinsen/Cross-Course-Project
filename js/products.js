98f43fe637f8da294d6cb9f4a7b3f3f84a0c21bd
MaxMartinsen <max.martinsen@hotmail.com>
2023-03-13 20:31:30 +0100

    Update products.js

--- js/products.js
+++ js/products.js
@@ -5,7 +5,7 @@ const products = [
         price: "8.9",
         description: "Boxer is a sports game that simulates the experience of being a professional boxer. In this game, players take on the role of a fighter and must train, strategize, and fight their way to the top of the rankings.",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/boxer-cover.jpg"
+        img: "/img/products/boxer-cover.jpg"
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ const products = [
         price: "8.9",
         description: "Wars between intelligence agencies are becoming more sophisticated: thanks to advanced technology, agents move into the bodies of other people and eliminate targets at the hands of relatives and relatives of the victims. Alexa learns that her husband, who is in a coma, is one of these sleeping agents, and tries to save ...",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/assassin-cover.jpg"
+        img: "/img/products/assassin-cover.jpg"
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ const products = [
         price: "10.9",
         description: "Black is an asymmetrical multiplayer survival horror game. A group of up to four survivors tries to escape from a nightmarish place where a supernaturally powerful killer tries to sacrifice them to a male ...",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/black-cover.jpg"
+        img: "/img/products/black-cover.jpg"
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ const products = [
         price: "8.9",
         description: "Cyberpunk is a genre of video game that typically takes place in a dystopian future where advanced technology has radically transformed society. These games often feature a mix of cybernetics, artificial intelligence, and advanced weaponry, creating a gritty and futuristic world where players take on the role of a protagonist fighting against corrupt corporations, gangs, or governments.",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/cyber-punk-cover.jpg"
+        img: "/img/products/cyber-punk-cover.jpg"
     },
     {
         id: 5,
@@ -37,7 +37,7 @@ const products = [
         price: "8.9",
         description: "Forge Legend is a multiplayer strategy game that puts players in charge of building and managing their own kingdom. In this game, players must gather resources, train soldiers, and expand their territory by conquering neighboring kingdoms.",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/forge-legend-cover.jpg"
+        img: "/img/products/forge-legend-cover.jpg"
     },
     {
         id: 6,
@@ -45,7 +45,7 @@ const products = [
         price: "8.9",
         description: "Furious is a survival horror video game developed and released by Endnight Games in 2023. It's an improved The Forest sequel, introducing friendly new NPCs, four times bigger map, and multiplayer for up to eig ...",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/furios-cover.jpg"
+        img: "/img/products/furios-cover.jpg"
     },
     {
         id: 7,
@@ -53,7 +53,7 @@ const products = [
         price: "4.9",
         description: "Ping pong, also known as table tennis, is a sports video game that simulates the experience of playing the popular indoor sport. In this game, players take on the role of a table tennis player and compete against other players or computer-controlled opponents in matches.",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/ping-pong-cover.jpg"
+        img: "/img/products/ping-pong-cover.jpg"
     },
     {
         id: 8,
@@ -61,7 +61,7 @@ const products = [
         price: "8.9",
         description: "Racing was developed by Italian studio Kumos Simulazioni. The developers focused on creating the most realistic racing simulator. In the game, players get access to over 170 officially licensed cars, including inn ...",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/racing-cover.jpg"
+        img: "/img/products/racing-cover.jpg"
     },
     {
         id: 9,
@@ -69,7 +69,7 @@ const products = [
         price: "8.9",
         description: "Space War is a science fiction video game that takes place in a distant future where humanity has colonized the stars and created a vast network of spaceships and space stations. In this game, players take on the role of a spaceship pilot and engage in epic battles against other pilots and alien forces.",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/space-war-cover.jpg"
+        img: "/img/products/space-war-cover.jpg"
     },
     {
         id: 10,
@@ -77,7 +77,7 @@ const products = [
         price: "4.9",
         description: "The sun begins to dawn on Velua, a new day for the world and a new day for you, a freshly minted Battle Researcher of the global research society known as Lux Solis. It’s your first day and everything goes smoothly until ...",
         brand: "Playbox",
-        img: "/GameHub-v2/img/products/super-duper-cover.jpg"
+        img: "/img/products/super-duper-cover.jpg"
     }
   ];
   
